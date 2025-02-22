2dc1cc8d470e9fcc493004b4b96e3457222204cd
Dany Oliva <dolivab@MacBook-Air-de-Dany.local>
2025-02-21 23:41:40 -0600

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hola desde el servidor Express')
+})
+
+app.listen(PORT, () => {
+    console.log('Hola desde el servidor Express')
+})
+
